fe8bd6bda2df32f9c56cdc2d58e75520a8236371
quickstart <quickstart@qs091.(none)>
2011-03-26 05:59:08 -0500

    Relative Pfade, alte Files umbewegen, falls schon vorhanden

--- alias.sh
+++ alias.sh
@@ -3,6 +3,10 @@
 #ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 #ln -s ~/.dotfiles/vim/.gvimrc ~/.gvimrc
 
-ln -s ~/.dotfiles/bash/.bashrc ~/.bashrc
-ln -s ~/.dotfiles/bash/.bash_logout ~/.bash_logout
-ln -s ~/.dotfiles/bash/.bash_aliases ~/.bash_aliases
+mv ~/.bashrc ~/.bashrc_backup
+mv ~/.bash_logout ~/.bash_logout_backup
+mv ~/.bash_aliases ~/.bash_aliases_backup
+
+ln -s $PWD/bash/.bashrc ~/.bashrc
+ln -s $PWD/bash/.bash_logout ~/.bash_logout
+ln -s $PWD/bash/.bash_aliases ~/.bash_aliases
